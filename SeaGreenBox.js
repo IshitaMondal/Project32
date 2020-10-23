87a8934bd086c64216809c6169104c21a1bf2f98
Ishita Mondal <70803670+IshitaMondal@users.noreply.github.com>
2020-10-23 18:18:32 +0530

    Add files via upload

---
+++ SeaGreenBox.js
@@ -0,0 +1,13 @@
+class Gbox extends BaseClass {
+    constructor(x, y, width, height){
+      super(x,y,width,height);
+      
+    }
+    display(){    
+      
+      fill(63,223,207);
+      super.display();
+    }
+  
+  };
+  
